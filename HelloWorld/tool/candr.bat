98522387409bb6a30e11c723c2f6fb567cbbdc63
klasing <klasing1959@gmail.com>
2017-01-14 15:29:34 +0100

    added jar to project

---
+++ HelloWorld/tool/candr.bat
@@ -0,0 +1,22 @@
+ECHO OFF
+
+REM go from subdirirectory named tool to the project root directory
+CD ..
+
+ECHO [COMPILE]
+C:\Progra~1\Java\jdk1.8.0_112\bin\javac src\*.java
+
+ECHO [DEPLOY]
+IF NOT EXIST build\src MKDIR build\src
+IF NOT EXIST build\src\resource MKDIR build\src\resource
+MOVE src\*.class build\src
+COPY src\resource\*.properties build\src\resource
+
+REM go from the project root directory to the subdirectory named build
+CD build
+
+ECHO [RUN]
+C:\Progra~1\Java\jdk1.8.0_112\bin\java src.HelloWorld es ES
+
+REM go from the subdirectory named build to the subdirectory named tool
+CD ..\tool
