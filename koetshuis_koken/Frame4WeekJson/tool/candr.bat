5839fb13e8eecee60acee4bb332b91b2b147bac9
klasing <klasing1959@gmail.com>
2017-02-17 06:25:39 +0100

    added servlet-api-2.5.jar
    When running there were complaints about a missing servlet class (in authorize method). After adding the jar the program works again.

--- koetshuis_koken/Frame4WeekJson/tool/candr.bat
+++ koetshuis_koken/Frame4WeekJson/tool/candr.bat
@@ -6,7 +6,7 @@ CD ..
 SET CLASSPATH=
 SET CLASSPATH=.\build;.\lib\jackson-annotations-2.8.0.jar;.\lib\jackson-core-2.8.1.jar;.\lib\jackson-databind-2.8.5.jar
 SET CLASSPATH=%CLASSPATH%;.\lib\google-api-services-drive-v3-rev55-1.22.0.jar;.\lib\google-oauth-client-java6-1.22.0.jar;.\lib\google-oauth-client-jetty-1.22.0.jar;.\lib\google-api-client-1.22.0.jar;.\lib\jetty-6.1.26.jar;.\lib\jetty-util-6.1.26.jar
-SET CLASSPATH=%CLASSPATH%;.\lib\google-api-client-1.22.0-sources.jar;.\lib\jackson-core-2.1.3.jar;.\lib\google-http-client-1.22.0.jar;.\lib\google-http-client-jackson2-1.22.0.jar;.\lib\google-oauth-client-1.22.0.jar
+SET CLASSPATH=%CLASSPATH%;.\lib\google-api-client-1.22.0-sources.jar;.\lib\jackson-core-2.1.3.jar;.\lib\google-http-client-1.22.0.jar;.\lib\google-http-client-jackson2-1.22.0.jar;.\lib\google-oauth-client-1.22.0.jar;.\lib\servlet-api-2.5.jar
 
 ECHO [COMPILE]
 %JAVA_HOME%javac src\*.java
